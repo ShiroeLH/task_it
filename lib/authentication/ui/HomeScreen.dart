26abd10c1196e478da5f50055824ae42116988d8
ShiroeLH <reychristian.eustaquio@yahoo.com>
2020-11-29 09:44:49 +0800

    Initial commit

---
+++ lib/authentication/ui/HomeScreen.dart
@@ -0,0 +1,107 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:task_it/authentication//constants.dart';
+import 'package:task_it/authentication/model/User.dart';
+import 'package:task_it/authentication/ui/AuthScreen.dart';
+import 'package:task_it/authentication/ui/Authenticate.dart';
+import 'package:task_it/authentication/ui/helper.dart';
+
+import '../handler.dart';
+
+FireStoreUtils _fireStoreUtils = FireStoreUtils();
+
+class HomeScreen extends StatefulWidget {
+  final User user;
+
+  HomeScreen({Key key, @required this.user}) : super(key: key);
+
+  @override
+  State createState() {
+    print(user.toString());
+    return _HomeState(user);
+  }
+}
+
+class _HomeState extends State<HomeScreen> {
+  final User user;
+
+  _HomeState(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(color: Colors.white),
+              ),
+              decoration: BoxDecoration(
+                color: Color(COLOR_PRIMARY),
+              ),
+            ),
+            ListTile(
+              title: Text(
+                'Logout',
+                style: TextStyle(color: Colors.black),
+              ),
+              leading: Transform.rotate(
+                  angle: pi / 1,
+                  child: Icon(Icons.exit_to_app, color: Colors.black)),
+              onTap: () async {
+                user.active = false;
+                user.lastOnlineTimestamp = Timestamp.now();
+                _fireStoreUtils.updateCurrentUser(user, context);
+                await FirebaseAuth.instance.signOut();
+                MyAppState.currentUser = null;
+                pushAndRemoveUntil(context, AuthScreen(), false);
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(color: Colors.black),
+        ),
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            displayCircleImage(user.profilePictureURL, 125, false),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(user.firstName),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(user.email),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(user.phoneNumber),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(user.userID),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/model/homelist.dart
@@ -0,0 +1,35 @@
+import 'package:task_it/design_course/home_design_course.dart';
+import 'package:task_it/fitness_app/fitness_app_home_screen.dart';
+import 'package:task_it/hotel_booking/hotel_home_screen.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeList {
+  HomeList({
+    this.navigateScreen,
+    this.imagePath = '',
+  });
+
+  Widget navigateScreen;
+  String imagePath;
+
+  static List<HomeList> homeList = [
+    HomeList(
+      imagePath: 'assets/task_manager/task_manager.png',
+      navigateScreen: DesignCourseHomeScreen(),
+    ),
+    /*
+    HomeList(
+      imagePath: 'assets/hotel/hotel_booking.png',
+      navigateScreen: HotelHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/fitness_app/fitness_app.png',
+      navigateScreen: FitnessAppHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/design_course/design_course.png',
+      navigateScreen: DesignCourseHomeScreen(),
+    ),
+     */
+  ];
+}
